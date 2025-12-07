abffc1ddb63d035da39586006f54c563ca9bc1e3
AAQ224 <AAQ_224@hotmail.com>
2025-12-07 20:37:23 +0300

    ready to deploy

--- EduGatePlatform/EduGatePlatform/settings.py
+++ EduGatePlatform/EduGatePlatform/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vibdmlr@ic=s=()@&$^m)@1nedtx@d$9=su(zk5-^#&m%l5vzz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,10 +81,20 @@ WSGI_APPLICATION = 'EduGatePlatform.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["PGDATABASE"],
+        'USER': os.environ["PGUSER"],
+        'PASSWORD': os.environ["PGPASSWORD"],
+        'HOST': os.environ["PGHOST"],
+        'PORT': os.environ["PGPORT"],
+    }
+} if not DEBUG else {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
@@ -121,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -145,4 +157,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "EMAIL_HOST_USER"
 EMAIL_HOST_PASSWORD = "EMAIL_HOST_PASSWORD"
 
-DEFAULT_FROM_EMAIL = 'EduGate Platform <xzxzd123@gmail.com>'
+DEFAULT_FROM_EMAIL = 'EduGate Platform <EMAIL_HOST_USER>'
